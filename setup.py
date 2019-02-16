c80b9cc4d52ae7ef3cf7eca96f08488296ae644c
EWouters <6179932+EWouters@users.noreply.github.com>
2019-02-16 10:27:39 +0100

    Add support for automated tests

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
+import sys
+
 from setuptools import setup
 
-setup(name='pydgilib',
-      version='0.1',
-      description='This module provides Python bindings for DGILib',
-      url='https://github.com/EWouters/Atmel-SAML11/tree/master/Python/pydgilib',
-      author='Erik Wouters',
-      author_email='ehwo(at)kth.se',
-      license='MIT',
-      packages=['pydgilib'],
-      dependency_links=['https://www.microchip.com/developmenttools/ProductDetails/ATPOWERDEBUGGER'],
-      zip_safe=False)
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+setup(
+    name="pydgilib",
+    version="0.1",
+    description="This module provides Python bindings for DGILib",
+    url="https://github.com/EWouters/Atmel-SAML11/tree/master/Python/pydgilib",
+    author="Erik Wouters",
+    author_email="ehwo(at)kth.se",
+    license="MIT",
+    packages=["pydgilib", "pydgilib_extra"],
+    dependency_links=[
+        "https://www.microchip.com/developmenttools/ProductDetails/ATPOWERDEBUGGER"
+    ],
+    zip_safe=False,
+    setup_requires=[
+        # ... (other setup requirements)
+    ]
+    + pytest_runner,
+    tests_require=["pytest"],
+)
---
+++ tests/test_pydgilib.py
@@ -0,0 +1,8 @@
+class TestPyDGILib(object):
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert True
