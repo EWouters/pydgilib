fa0d8c5bfd3fbba29d5842abc195e6deddb93f58
EWouters <6179932+EWouters@users.noreply.github.com>
2019-02-28 09:29:57 +0100

    Add unit test project to be loaded on the board when running automated tests for the library
    Supports printing over serial
    Flips the gpio pin values and blinks the led

---
+++ UnitTest/UnitTest/main.c
@@ -0,0 +1,21 @@
+#include <atmel_start.h>
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+
+	/* Replace with your application code */
+	while (1) {
+		delay_ms(100);
+		gpio_toggle_pin_level(DGI_GPIO0);
+		delay_ms(10);
+		gpio_toggle_pin_level(DGI_GPIO1);
+		delay_ms(10);
+		gpio_toggle_pin_level(DGI_GPIO2);
+		delay_ms(10);
+		gpio_set_pin_level(DGI_GPIO3, GPIO_HIGH);
+		gpio_toggle_pin_level(LED0);
+		gpio_set_pin_level(DGI_GPIO3, GPIO_LOW);
+	}
+}
