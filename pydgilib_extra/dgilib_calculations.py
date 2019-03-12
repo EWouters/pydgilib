b90099cee237c02860d966d5be86d5cb03b5314d
EWouters <6179932+EWouters@users.noreply.github.com>
2019-03-12 11:37:21 +0100

    Put standalone gpio_augment_edges back in (was not supposed to be commented)

--- pydgilib_extra/dgilib_calculations.py
+++ pydgilib_extra/dgilib_calculations.py
@@ -76,58 +76,58 @@ class GPIOAugmentEdges(StreamingCalculation):
         return gpio_data
 
 
-# def gpio_augment_edges(gpio_data, delay_time=0, switch_time=0, extend_to=None):
-#     """GPIO Augment Edges (standalone).
+def gpio_augment_edges(gpio_data, delay_time=0, switch_time=0, extend_to=None):
+    """GPIO Augment Edges (standalone).
 
-#     Augments the edges of the GPIO data by inserting an extra sample of the
-#     previous pin values at moment before a switch occurs (minus switch_time).
-#     The switch time is measured to be around 0.3 ms.
+    Augments the edges of the GPIO data by inserting an extra sample of the
+    previous pin values at moment before a switch occurs (minus switch_time).
+    The switch time is measured to be around 0.3 ms.
 
-#     Also delays all time stamps by delay_time. The delay time seems to vary
-#     a lot between different projects and should be manually specified for the
-#     best accuracy.
+    Also delays all time stamps by delay_time. The delay time seems to vary
+    a lot between different projects and should be manually specified for the
+    best accuracy.
 
-#     Can insert the last datapoint again at the time specified (has to be after
-#     last sample).
+    Can insert the last datapoint again at the time specified (has to be after
+    last sample).
 
-#     :param gpio_data: InterfaceData object of GPIO data.
-#     :type gpio_data: InterfaceData
-#     :param delay_time: Switch time of GPIO pin.
-#     :type delay_time: float
-#     :param switch_time: Switch time of GPIO pin.
-#     :type switch_time: float
-#     :param extend_to: Inserts the last pin values again at the time specified
-#         (only used if time is after last sample).
-#     :type extend_to: float
-#     :return: InterfaceData object of augmented GPIO data.
-#     :rtype: InterfaceData
-#     """
-#     pin_states = [False] * NUM_PINS
+    :param gpio_data: InterfaceData object of GPIO data.
+    :type gpio_data: InterfaceData
+    :param delay_time: Switch time of GPIO pin.
+    :type delay_time: float
+    :param switch_time: Switch time of GPIO pin.
+    :type switch_time: float
+    :param extend_to: Inserts the last pin values again at the time specified
+        (only used if time is after last sample).
+    :type extend_to: float
+    :return: InterfaceData object of augmented GPIO data.
+    :rtype: InterfaceData
+    """
+    pin_states = [False] * NUM_PINS
 
-#     # iterate over the list and insert items at the same time:
-#     i = 0
-#     while i < len(gpio_data.timestamps):
-#         if gpio_data.values[i] != pin_states:
-#             # This inserts a time sample at time + switch time (so moves the
-#             # time stamp into the future)
-#             gpio_data.timestamps.insert(
-#                 i, gpio_data.timestamps[i] - switch_time)
-#             # This inserts the last datapoint again at the time the next
-#             # switch actually arrived (without switch time)
-#             gpio_data.values.insert(i, pin_states)
-#             i += 1
-#             pin_states = gpio_data.values[i]
-#         i += 1
+    # iterate over the list and insert items at the same time:
+    i = 0
+    while i < len(gpio_data.timestamps):
+        if gpio_data.values[i] != pin_states:
+            # This inserts a time sample at time + switch time (so moves the
+            # time stamp into the future)
+            gpio_data.timestamps.insert(
+                i, gpio_data.timestamps[i] - switch_time)
+            # This inserts the last datapoint again at the time the next
+            # switch actually arrived (without switch time)
+            gpio_data.values.insert(i, pin_states)
+            i += 1
+            pin_states = gpio_data.values[i]
+        i += 1
 
-#     # Delay all time stamps by delay_time
-#     gpio_data.timestamps = [
-#         t + delay_time for t in gpio_data.timestamps]
+    # Delay all time stamps by delay_time
+    gpio_data.timestamps = [
+        t + delay_time for t in gpio_data.timestamps]
 
-#     if extend_to is not None:
-#         if extend_to >= gpio_data.timestamps[-1]:
-#             gpio_data.timestamps.append(extend_to)
-#             gpio_data.values.append(pin_states)
-#     return gpio_data
+    if extend_to is not None:
+        if extend_to >= gpio_data.timestamps[-1]:
+            gpio_data.timestamps.append(extend_to)
+            gpio_data.values.append(pin_states)
+    return gpio_data
 
 
 def power_and_time_per_pulse(logger_data, pin, start_time=0.01, end_time=None,
